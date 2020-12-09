37dcc65adbcb8586f34b590eb8b0b51b8c54cb89
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2020-12-09 16:55:36 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     mongodbMemoryServerOptions: {
+      instance: {
+          dbName: 'jest'
+      },
       binary: {
-        version: '4.0.3',
+        version: '4.2.3',
         skipMD5: true
       },
       autoStart: false,
-      instance: {
-          dbName: 'jest'
-      }
     }
   };
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,6 +1,4 @@
-import { connect } from "http2";
-
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
     client: null as MongoClient,
@@ -11,7 +9,12 @@ export const MongoHelper = {
             useUnifiedTopology: true
         })
     },
+
     async disconnect (): Promise<void> {
         await this.client.close()
+    },
+
+    getCollection (name: string): Collection {
+        return this.client.db().collection(name)
     }
 }
