3279d73b876fc97957269c318951e9714d12d466
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2020-12-09 14:35:26 -0300

    refactor: move AddAccountRepository import to a single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,6 +1,4 @@
-import { AddAccountRepository } from "../../protocols/add-account-repository"
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from "./db-add-account.protocols"
-
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Encrypter } from "./db-add-account.protocols"
 
 export class DbAddAccount implements AddAccount {
     private readonly encrypter: Encrypter
