195f34eda0903370070cd2725cb5e726a979162a
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2020-12-09 21:48:49 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,19 +1,33 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
-      app.get('/test_cors', (req, res) => {
-          res.send()
-      })
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
     await request(app)
-        .post('/api/signup')
-        .send({
-            name: 'Lucas',
-            email: 'lucas.liima030@gmail.com',
-            password: '123',
-            passwordConfirmation: '123'
-        })
-        .expect(200)
+      .post('/api/signup')
+      .send({
+        name: 'Lucas',
+        email: 'lucas.liima030@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
   })
 })
